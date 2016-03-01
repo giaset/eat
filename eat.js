8e9ab5c7db6bdc063c9379a738df17df1f612286
Gianni Settino <g@miltonandparc.com>
2016-03-01 14:53:43 -0500

    Setup web app

--- eat.js
+++ eat.js
@@ -1,1 +1,13 @@
 var twilio = require('twilio')('AC5b415a7ef6329a850e7ed759774a12c1', 'b23caaa81a1195231bde48dc6d2af01b')
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.get('/', function(request, response) {
+	response.send('EAT SHIT')
+})
+
+app.listen(app.get('port'), function() {
+	console.log('App is running on port', app.get('port'))
+})
